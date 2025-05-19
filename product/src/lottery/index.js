ba88762524d7b945172153102bb0319de35dcf95
lesile_lee <lesile_lee@163.com>
2025-05-19 12:42:03 +0800

    feature: 优化

--- product/src/lottery/index.js
+++ product/src/lottery/index.js
@@ -57,9 +57,9 @@ let selectedCardIndex = [],
   isLotting = false,
   currentLuckys = [];
 
-if (!window.localStorage.getItem("lotteryData") === 'dds') {
+if (!window.localStorage.getItem("lotteryData") !== 'dds') {
   const input = prompt('请输入密码');
-  if (input !== 'sssz17') {
+  if (input === 'sssz17') {
     window.localStorage.setItem("lotteryData", 'dds');
     initAll();
   } else {
